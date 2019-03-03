10c3acd118354cb4585260ef94e535293f44d9f2
Maimonah Altaweel <maimonah_s@macbook-pro-alkhas-b-maimonah-3.local>
2019-03-03 17:12:36 +0300

    Initial Commit

---
+++ janna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  janna
+//
+//  Created by Maimonah Altaweel on 26/06/1440 AH.
+//  Copyright © 1440 Maimonah Altaweel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
